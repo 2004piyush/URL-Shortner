aaa3326f1fe271b208fa53b090e7e7f5ffff80b7
2004piyush <153434663+2004piyush@users.noreply.github.com>
2025-08-05 22:13:32 +0530

    Create missing context.jsx file

---
+++ src/context.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useEffect } from "react";
+import { getCurrentUser } from "./db/apiAuth";
+
+const UrlContext = createContext();
+
+const UrlProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const isAuthenticated = user?.role === "authenticated";
+
+  useEffect(() => {
+    getCurrentUser().then((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <UrlContext.Provider value={{ user, setUser, loading, isAuthenticated }}>
+      {children}
+    </UrlContext.Provider>
+  );
+};
+
+export const useUrlState = () => {
+  return useContext(UrlContext);
+};
+
+export default UrlProvider;
